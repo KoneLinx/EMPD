8d3997b33598642bef0ab012bb974503a2c07dad
KoneLinx <konelinx@posteo.net>
2018-01-20 20:58:08 +0100

    ytsearch1.2(0)

--- SearchYT.py
+++ SearchYT.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python
 
-# sample executes a search request for the specified search term.
-# Sample usage:
-#   python search.py --q=surfing --max-results=10
-# NOTE: To use the sample, you must provide a developer key obtained
-#       in the Google APIs Console. Search for "REPLACE_ME" in this code
-#       to find the correct place to provide that key..
-
 import argparse
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-
-# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
-# tab of
-#   https://cloud.google.com/console
-# Please ensure that you have enabled the YouTube Data API for your project.
 DEVELOPER_KEY = 'AIzaSyDwdFkHu7_dnIjU_KWw2pyUYKrm0EjKmbY'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
@@ -25,8 +13,6 @@ def youtube_search(options):
   youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     developerKey=DEVELOPER_KEY)
 
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
   search_response = youtube.search().list(
     q=options.q,
     part='id,snippet',
@@ -35,8 +21,6 @@ def youtube_search(options):
 
   videos = []
 
-  # Add each result to the appropriate list, and then display the lists of
-  # matching videos, channels, and playlists.
   for search_result in search_response.get('items', []):
     if search_result['id']['kind'] == 'youtube#video':
       videos.append('--id %s --name %s' % (search_result['id']['videoId'],
